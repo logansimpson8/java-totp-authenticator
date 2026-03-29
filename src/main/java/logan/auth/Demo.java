1c43d62697c43e735626749773986b492a0436f2
Logan Simpson <logansimpson136@gmail.com>
2026-03-29 13:52:44 +0100

    fixed the demo to be better displayed

--- src/main/java/logan/auth/Demo.java
+++ src/main/java/logan/auth/Demo.java
@@ -1,21 +1,35 @@
 package logan.auth;
+import java.util.concurrent.TimeUnit;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
         Authenticator auth = new Authenticator();
         TimeProvider time = new TimeProvider();
-        String userSecret = StringCodeGenerator.generateSecret();
+        MathEngine engine = new MathEngine();
 
-        System.out.println("demo");
-        System.out.println("Secret (Base32) string: " + userSecret);
-        System.out.println(" ");
+        //String userSecret = StringCodeGenerator.generateSecret(); - use this to generate a new secret.
+        String userSecret = "YDWFYTFRGTA6FGTZJJ5DQBCMFNKEPFCN";
+        // Im leaving the secret here for demo purposes so the system dosent keep resetting the google auth
+        // when actually using, store the code securley.
 
-        for (int i = 0; i < 60; i++) {
+        System.out.println("Master Secret: " + userSecret);
+        System.out.println("enter it into Google Authenticator.");
+        System.out.println("-------------------------------------------");
+        System.out.println("live codes (Press Ctrl+C to stop monitoring)");
+        System.out.println();
+
+        while (true) {
             long step = time.getCurrentStep();
-            MathEngine engine = new MathEngine();
             int currentPin = engine.calculateCode(userSecret, step);
-            System.out.printf("TimeStep (not relevant to user): " + step + "currentPin: " + currentPin );
-            Thread.sleep(30000);
+
+            long unixTime = System.currentTimeMillis() / 1000;
+            long secondsLeft = 30 - (unixTime % 30);
+            System.out.printf("\r[ CURRENT PIN: %06d ]  Expires in: %2ds ", currentPin, secondsLeft);
+            Thread.sleep(1000);
+
+            if (secondsLeft == 1) {
+                System.out.print("\n--- Pin REFRESHED ---\n");
+            }
         }
     }
 }
